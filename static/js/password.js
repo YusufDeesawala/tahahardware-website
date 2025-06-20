7f7b501774a80e2c6ef36fefc88a8b894196ffd7
YusufDeesawala <yusufdeesawala72@gmail.com>
2025-06-20 16:18:21 +0530

    Removed all files.

--- static/js/password.js
+++
@@ -1,8 +0,0 @@
-function checkPassword(correctPassword) {
-    const password = prompt('Enter the password to view client details:');
-    if (password === correctPassword) {
-      document.getElementById('content').style.display = 'block';
-    } else {
-      document.getElementById('error').style.display = 'block';
-    }
-  }
